722baeb042ab68915eca5bc507893c6b88fc73c9
Beater <trunghieu14092004@gmail.com>
2024-10-01 21:44:26 +0700

    Completed the first two topics.

---
+++ De1/Program.cs
@@ -0,0 +1,47 @@
+﻿//----------------------------Câu 1----------------------------//
+class Animal{
+    internal String Name { get => Name; set => Name = value; }
+    internal int Age { get => Age; set => Age = value; }
+
+    internal void DisplayInfo(){
+        Console.WriteLine("Name: " + Name + "\nAge: " + Age);
+    }
+}
+class Dog : Animal{
+    internal String Breed { get => Breed; set => Breed = value; }
+}
+//-------------------------------------------------------------//
+
+//----------------------------Câu 2----------------------------//
+public delegate int Operation(int x, int y);
+public class Cau2{
+    internal int Add(int x, int y){
+        return x + y;
+    }
+}
+//-------------------------------------------------------------//
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lựa chọn: ");
+        int lc = int.Parse(Console.ReadLine());
+        if (lc == 1){
+            Dog myDog = new Dog();
+            myDog.Name = "Den";
+            myDog.Age = 5;
+            myDog.Breed = "I don't know";
+            myDog.DisplayInfo();
+        }
+        else if (lc == 2) {
+            Cau2 c2 = new Cau2();
+            Operation opera = new Operation(c2.Add);
+            Console.Write("Nhập x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nhập y: ");
+            int y = int.Parse(Console.ReadLine());
+            int result = opera(x, y);
+            Console.WriteLine("Đáp án: " + result);
+        }
+    }
+}
---
+++ De2/Program.cs
@@ -0,0 +1,45 @@
+﻿//----------------------------Câu 1----------------------------//
+class Shape{
+    internal virtual void Draw(){}
+}
+
+class Circle : Shape{
+    internal override void Draw(){
+        Console.WriteLine("Vẽ hình tròn");
+    }
+}
+
+class Rectangle : Shape{
+    internal override void Draw(){
+        Console.WriteLine("Vẽ hình chữ nhật");
+    }
+}
+//-------------------------------------------------------------//
+
+//----------------------------Câu 2----------------------------//
+class Task{
+    public event EventHandler OnCompleted;
+    internal void CompleteTask(){
+        OnCompleted?.Invoke(this, EventArgs.Empty);
+    }
+}
+//-------------------------------------------------------------//
+class Program{
+    static void Main(string[] args){
+        Console.Write("Lựa chọn: ");
+        int lc = int.Parse(Console.ReadLine());
+        if (lc == 1){
+            Shape circle = new Circle();
+            Shape rectangle = new Rectangle();
+            circle.Draw();
+            rectangle.Draw();
+        }
+        else if (lc == 2) {
+            Task task = new Task();
+            task.OnCompleted += (sender, e) => {
+                Console.WriteLine("Task đã hoàn thành");
+            };
+            task.CompleteTask();
+        }
+    }
+}
